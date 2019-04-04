74249a81af35842908b9252d050ad4996a53b55d
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-04 17:56:27 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(val)
+  #first test
+  
+  if val % 5 == 0 && val % 3 == 0  
+    "FizzBuzz"
+  elsif val % 5 == 0 
+     "Buzz"
+  elsif val % 3 == 0 
+     "Fizz"
+  else
+    nil
+  end
+  
+end
