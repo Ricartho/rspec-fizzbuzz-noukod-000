fdec9eb6791759f894a0cab1590632259085273e
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-04 17:46:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(val)
+  #first test
+  if val % 3 == 0 
+    "Fizz"
+  elsif val % 5 == 0 
+     "Buzz"
+  elsif val % 3 && val % 5 ==0 
+      "FizzBuzz"
+  end
+  
+end
